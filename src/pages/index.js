c60c8aa6fdb0c009e264d6837b94d367aa2f59ec
Margarita Amy Wang <margaritaawang@gmail.com>
2020-01-28 00:19:06 -0800

    template for about and header section

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,22 @@
 import React from "react"
 import { ThemeProvider } from 'emotion-theming'
 import theme from '../theme'
+import { Flex } from "rebass";
 import Navbar from '../Components/navbar';
+import About from '../Components/about';
+import Header from '../Components/header';
+import backgroundImage from '../images/dark-purple-blurred-water.jpg';
 
 export default (props) => (
 
 <ThemeProvider theme={theme}>
+  <Flex flexDirection="column" sx={{
+        backgroundImage: `url(${backgroundImage})`,
+        backgroundSize: 'cover',
+        height: '100vh'
+    }}>
     <Navbar/>
-    <h1>Hello Gatsby!</h1>
+    <Header/>
+    </Flex>
+    <About/>
   </ThemeProvider>)
--- src/theme.js
+++ src/theme.js
@@ -43,7 +43,7 @@ export default {
     buttons: {
       primary: {
         color: 'white',
-        bg: 'primary',
+        bg: 'secondary',
       }
     }
   }
